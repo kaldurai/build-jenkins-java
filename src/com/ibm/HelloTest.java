bace744e7d74b94c50171168c06837172eea9d98
kalai <kaldurai@in.ibm.com>
2017-11-21 03:12:06 -0600

    new files

---
+++ src/com/ibm/HelloTest.java
@@ -0,0 +1,11 @@
+package com.ibm;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Hello... Test Project to Build Java project in Jenkins-Git");
+  System.out.println("**** Successfully tested!! ********** ");
+	}
+
+}
